8fc8ec016d4fd087ecfb677c641ba024ffc646dd
SEBASTIAN <sebastian.rincon13@gmail.com>
2014-10-30 17:17:58 -0500

    30-10-14

--- DePlan2/src/com/appAndroid/deplan/Lista_contenido.java
+++ DePlan2/src/com/appAndroid/deplan/Lista_contenido.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import android.net.Uri;
 import android.widget.ImageButton;
 
 import com.appAndroid.deplan.R;
@@ -35,12 +36,12 @@ public class Lista_contenido {
 	 */
 	static {
 		aniadirEntrada(new Lista_entrada("0","DESFILE INAGURAL", R.drawable.afiche_opt, "31 de octubre de 2014, 10:00 AM", "Plaza de Bolívar de Tunja", "Entrada Libre"));
-		aniadirEntrada(new Lista_entrada("1","JUANES EN CONCIERTO", R.drawable.juanes_opt, "8 de noviembre de 2014, 7:00 PM", "Estadio la Independencia Tunja", "Entrada Libre"));
+		aniadirEntrada(new Lista_entrada("1","JUANES EN CONCIERTO", R.drawable.juanes_opt, "8 de noviembre de 2014, 7:00 PM", "Estadio la Independencia Tunja", "$33.000 General, $55.000 VIP"));
 		aniadirEntrada(new Lista_entrada("2","JOVENES Y PENSAMIENTO", R.drawable.jovenes_opt, "1 de noviembre de 2014, 7:00 PM", "Coliseo cubierto de Tunja", "$11.000 - General"));
-		aniadirEntrada(new Lista_entrada("3","TABLADO, FLAMENCO, TANGO Y CHACARERA", R.drawable.tablado_opt, "3 de noviembre de 2014, 7:00 PM", "Coliseo cubierto de Tunja", "Entrada Libre"));
-		aniadirEntrada(new Lista_entrada("4","MUJERES LATINOAMERICANAS EN CONCIERTO", R.drawable.mujeres_opt, "4 de noviembre de 2014, 7:00 PM", "Coliseo cubierto de Tunja", "Entrada Libre"));
-		aniadirEntrada(new Lista_entrada("5","AÑOS DORADOS DE LA MUSICA ROMANTICA", R.drawable.nochesestelares_opt, "5 de noviembre de 2014, 7:00 PM", "Coliseo cubierto de Tunja", "Entrada Libre"));
-		aniadirEntrada(new Lista_entrada("6","FOLCLOR MEXICANO", R.drawable.mariachi_opt, "6 de noviembre de 2014, 7:00 PM", "Coliseo cubierto de Tunja", "Entrada Libre"));
+		aniadirEntrada(new Lista_entrada("3","TABLADO, FLAMENCO, TANGO Y CHACARERA", R.drawable.tablado_opt, "3 de noviembre de 2014, 7:00 PM", "Coliseo cubierto de Tunja", "$11.000 General, $66.000-$88.000 VIP"));
+		aniadirEntrada(new Lista_entrada("4","MUJERES LATINOAMERICANAS EN CONCIERTO", R.drawable.mujeres_opt, "4 de noviembre de 2014, 7:00 PM", "Coliseo cubierto de Tunja", "$16.500 General, $88.000-$110.000 VIP"));
+		aniadirEntrada(new Lista_entrada("5","AÑOS DORADOS DE LA MUSICA ROMANTICA", R.drawable.nochesestelares_opt, "5 de noviembre de 2014, 7:00 PM", "Coliseo cubierto de Tunja", "$16.500 General, $88.000-$110.000 VIP"));
+		aniadirEntrada(new Lista_entrada("6","FOLCLOR MEXICANO", R.drawable.mariachi_opt, "6 de noviembre de 2014, 7:00 PM", "Coliseo cubierto de Tunja", "$11.000 General, $66.000-$88.000 VIP"));
 		aniadirEntrada(new Lista_entrada("7", "DANZA FOLCLORICA", R.drawable.danza, "29 y 30 de octubre, 10:00 AM", "Casa Cultural Enrique Medina Flores", "Entrada Libre"));
 		aniadirEntrada(new Lista_entrada("8","FORO - TALLER Danza Folclórica", 
 				R.drawable.danza, "29 y 30 de octubre de 2014, 2:00 PM", 
@@ -149,6 +150,161 @@ public class Lista_contenido {
 				aniadirEntrada(new Lista_entrada("34","PELÍCULA DE BRASIL – Malu de Dicicleta", 
 					R.drawable.cine, "1 de noviembre de 2014, 7:00 PM", 
 					"Plazoleta Multiparque Cooservicios", "Entrada Libre"));
+				
+				aniadirEntrada(new Lista_entrada("35","GRUPO DE TEATRO  PLÉYADE – Teatro Clow, Obra: Joker y/o comodín", 
+						R.drawable.teatro, "2 de noviembre de 2014, 10:00 AM", 
+						"Teatro Cultural", "Entrada Libre"));
+
+					aniadirEntrada(new Lista_entrada("36","GRABACIÓN PRIMER TRAILER ANIMADO EN BOYACÁ ALIENT BOYACENSE", 
+						R.drawable.cine, "2 de noviembre de 2014, 10:00 AM", 
+						"Plaza de Bolívar", "Entrada Libre"));
+
+					aniadirEntrada(new Lista_entrada("37","LITERATURA Y ACADEMIA CONFERENCIA URY RODRIGUEZ – CUBA", 
+						R.drawable.literatura, "2 de noviembre de 2014, 10:00 AM", 
+						"Auditorio Eduardo Caballero Calderón, Sec de Cultura y Turismo de Boyacá Tunja", "Entrada Libre"));
+
+					aniadirEntrada(new Lista_entrada("38","TARDE DE ROCK Y BOYACÁ ROCK", 
+						R.drawable.rock_opt, "2 de noviembre de 2014, 10:00 AM", 
+						"Coliseo Cubierto", "Entrada Libre")); 
+					
+					aniadirEntrada(new Lista_entrada("39","CONVERSATORIO ANDRÉ MATTOS. BRASIL. TEATRO CON NIÑOS", 
+							R.drawable.literatura, "2 de noviembre de 2014, 3:00 PM", 
+							"Salón de la Constitución, Gobernacion de Boyacá", "Entrada Libre"));
+
+						aniadirEntrada(new Lista_entrada("40","ASOCIACIÓN DE LITERATURA: ANTOLOGÍA BOSA", 
+							R.drawable.literatura, "2 de noviembre de 2014, 3:00 PM", 
+							"Auditorio Eduardo Caballero Calderón, Sec de Cultura y Turismo de Boyacá Tunja", "Entrada Libre"));
+
+						aniadirEntrada(new Lista_entrada("41","CONCIERTO BLANCO: NATHAN IRONSIDE–AUSTRALIA, GIOVANNI HERNÁNDEZ, DANIEL MENDOZA", 
+							R.drawable.musica, "2 de noviembre de 2014, 3:00 PM", 
+							"Plaza de Bolívar", "Entrada Libre"));
+
+						aniadirEntrada(new Lista_entrada("42","CONCIERTO CUERDA Y VOZ: MARTÍN TRIO–VENEZUELA", 
+							R.drawable.musica, "2 de noviembre de 2014, 3:00 PM", 
+							"Auditorio José Mosser", "Entrada Libre"));
+
+						aniadirEntrada(new Lista_entrada("43","FUNCIÓN ESTELAR DE TEATRO Obra:PHARMAKON, Protagonista ALEJANDRA BORRERO", 
+							R.drawable.teatro, "2 de noviembre de 2014, 7:00 PM", 
+							"Teatro Maldonado", "$16.500 - General"));
+
+						aniadirEntrada(new Lista_entrada("44","TEATRO CUENTAN LOS QUE CUENTAN, LUIS GABRIEL MORENO MURCIA", 
+							R.drawable.teatro, "2 de noviembre de 2014, 7:00 PM", 
+							"Auditorio Eduardo Caballero Calderón, Sec de Cultura y Turismo de Boyacá, Tunja", "Entrada Libre"));
+
+						aniadirEntrada(new Lista_entrada("45","ESTELAR DE DANZA: CRÒNICA DE UNA OBRA DANZADA: L´EXPLOSE", 
+							R.drawable.danza, "2 de noviembre de 2014, 7:00 PM", 
+							"Teatro Suarez", "$11.000 - General"));
+
+						aniadirEntrada(new Lista_entrada("46","TALLER DE FOTOGRAFÍA Y VIDEO: POR LOS SENDEROS DE LA FOTOGRAFÍA", 
+							R.drawable.cine, "3 de noviembre de 2014, 10:00 AM", 
+							"Plaza de Bolívar", "Entrada Libre"));
+
+						aniadirEntrada(new Lista_entrada("47","GRUPO DE TEATRO MAGIA ROJA, Obra: La Madriguera", 
+							R.drawable.teatro, "3 de noviembre de 2014, 10:00 AM", 
+							"Teatro Cultural", "Entrada Libre"));
+
+						aniadirEntrada(new Lista_entrada("48","DANZAS – ACADEMIA TALLER: GRUPO DE DANZAS BARRANQUILLA", 
+							R.drawable.danza, "3 de noviembre de 2014, 10:00 AM", 
+							"Casa Cultural Enrique Medina Flórez, Calle 19 No, 12-54", "Entrada Libre"));
+
+						aniadirEntrada(new Lista_entrada("49","TARDE DE JÓVENES CAMPESINOS Y EXPRESIONES AUTÓCTONAS", 
+							R.drawable.musica, "3 de noviembre de 2014, 3:00 PM", 
+							"Plaza de Bolívar", "Entrada Libre"));
+
+						aniadirEntrada(new Lista_entrada("50","CLÁSICOS DE LA MÚSICA Y LA DANZA", 
+							R.drawable.musica, "3 de noviembre de 2014, 3:00 PM", 
+							"Auditorio José Mosser", "Entrada Libre"));
+
+						aniadirEntrada(new Lista_entrada("51","DANZAS COLOMBIA BALLET FOLCLÓRICO DE CUNDINAMARCA", 
+							R.drawable.danza, "3 de noviembre de 2014, 3:00 PM", 
+							"Teatro Maldonado", "Entrada Libre"));
+
+						aniadirEntrada(new Lista_entrada("52","VOCES LÍRICAS DEL MUNDO", 
+							R.drawable.musica, "3 de noviembre de 2014, 7:00 PM", 
+							"Templo de San Ignacio", "Entrada Libre"));
+
+						aniadirEntrada(new Lista_entrada("53","GABOCO–GRUPO DE DANZAS GARAGOA", 
+							R.drawable.danza, "3 de noviembre de 2014, 7:00 PM", 
+							"Teatro Suarez", "Entrada Libre"));
+
+						aniadirEntrada(new Lista_entrada("54","FUNCIÓN ESTELAR DE DANZAS: DANZA INTERNACIONAL–BRASIL", 
+							R.drawable.danza, "3 de noviembre de 2014, 7:00 PM", 
+							"Teatro Maldonado", "Entrada Libre"));
+
+						aniadirEntrada(new Lista_entrada("55","FUNCIÓN ESTELAR DE DANZAS: GRUPO DE DANZAS UNIVERSIDAD DEL ATLÁNTICO", 
+							R.drawable.danza, "3 de noviembre de 2014, 7:00 PM", 
+							"Teatro Maldonado", "Entrada Libre"));
+
+						aniadirEntrada(new Lista_entrada("56","TEATRO CUENTAN LOS QUE CUENTAN: NELSON LÓPEZ", 
+							R.drawable.teatro, "3 de noviembre de 2014, 7:00 PM", 
+							"Auditorio Eduardo Caballero Calderón, Sec de Cultura y Turismo de Boyacá Tunja", "Entrada Libre"));
+
+						aniadirEntrada(new Lista_entrada("57","LA TRAMOYA–TEATRO NACIONAL Obra: El Hombre Reina y la Mujer Gobierna", 
+							R.drawable.teatro, "3 de noviembre de 2014, 7:00 PM", 
+							"Teatro Cultural", "$11.000 General"));
+						aniadirEntrada(new Lista_entrada("58","PREMIACIÓN TERCER ENCUENTRO DEPARTAMENTAL DEL CUENTO INFANTIL", 
+								R.drawable.literatura, "4 de noviembre de 2014, 10:00 AM", 
+								"Salón de la Constitución, Gobernacion de Boyacá", "Entrada Libre"));
+
+							aniadirEntrada(new Lista_entrada("59","TALLER DE MONTAJE. EDICIÒN SISTEMA ADOBE AFTER EFFECTS", 
+								R.drawable.cine, "4 de noviembre de 2014, 10:00 AM", 
+								"Teatro Suárez", "Entrada Libre"));
+
+							aniadirEntrada(new Lista_entrada("60","LANZAMIENTO DE OBRA: EUTIMIO REYES: ESCRITOR BOYACENSE", 
+								R.drawable.literatura, "4 de noviembre de 2014, 10:00 AM", 
+								"Auditorio Eduardo Caballero Calderón, Sec de Cultura y Turismo de Boyacá Tunja", "Entrada Libre"));
+
+							aniadirEntrada(new Lista_entrada("61","TALLER DE SAMBA Y DANZA DEL FOLCLOR DEL  BRASIL", 
+								R.drawable.danza, "4 de noviembre de 2014, 10:00 AM", 
+								"Casa Cultural Enrique Medina Flórez. Calle 19 No. 12-54", "Entrada Libre"));
+
+							aniadirEntrada(new Lista_entrada("62","GRUPO DE TEATRO ALMEIDA, Obra: Colcha de Risas", 
+								R.drawable.teatro, "4 de noviembre de 2014, 10:00 AM", 
+								"Teatro Cultural", "Entrada Libre"));
+
+							aniadirEntrada(new Lista_entrada("63","PRIMER FESTIVAL BOYACENSE  DE POESÍA COSTUMBRISTA", 
+								R.drawable.literatura, "4 de noviembre de 2014, 2:00 PM", 
+								"Auditorio José Mosser", "Entrada Libre"));
+
+							aniadirEntrada(new Lista_entrada("64","TALLER DE MONTAJE: MUSICALIZACIÓN Y EFECTOS DE SONIDO", 
+								R.drawable.cine, "4 de noviembre de 2014, 2:00 PM", 
+								"Teatro  Suarez", "Entrada Libre"));
+
+							aniadirEntrada(new Lista_entrada("65","RECITAL POÉTICO", 
+								R.drawable.literatura, "4 de noviembre de 2014, 3:00 PM", 
+								"Auditorio Eduardo Caballero Calderón, Sec de Cultura y Turismo de Boyacá Tunja", "Entrada Libre"));
+
+							aniadirEntrada(new Lista_entrada("66","CONFERENCIA: DESARROLLO DE LA MÚSICA GREGORIANA EN VENEZUELA", 
+								R.drawable.literatura, "4 de noviembre de 2014, 3:00 PM", 
+								"Salón de la Constitución, Gobernacion de Boyacá", "Entrada Libre"));
+
+							aniadirEntrada(new Lista_entrada("67","TARDE DE TRIOS Y CUERDAS", 
+								R.drawable.musica, "4 de noviembre de 2014, 3:00 PM", 
+								"Plaza de Bolívar", "Entrada Libre"));
+
+							aniadirEntrada(new Lista_entrada("68","PELÍCULA SARA Y PELÍCULA PUNTO CRÍTICO", 
+								R.drawable.cine, "4 de noviembre de 2014, 6:00 PM", 
+								"Teatro Suarez", "Entrada Libre"));
+
+							aniadirEntrada(new Lista_entrada("69","CONCIERTO VIENTOS Y SUR", 
+								R.drawable.musica, "4 de noviembre de 2014, 7:00 PM", 
+								"Templo de San Ignacio", "Entrada Libre"));
+
+							aniadirEntrada(new Lista_entrada("70","BALLET ANDINO DE ECUADOR", 
+								R.drawable.danza, "4 de noviembre de 2014, 7:00 PM", 
+								"Teatro Maldonado", "Entrada Libre"));
+
+							aniadirEntrada(new Lista_entrada("71","CORPOESFERA DANZA EXPERIMENTAL CONTEMPORÁNEA–BUCARAMANGA", 
+								R.drawable.danza, "4 de noviembre de 2014, 7:00 PM", 
+								"Teatro Maldonado", "Entrada Libre"));
+
+							aniadirEntrada(new Lista_entrada("72","TEATRO CUENTAN LOS QUE CUENTAN, RAMSÉS MOCTEZUMA", 
+								R.drawable.teatro, "4 de noviembre de 2014, 7:00 PM", 
+								"Auditorio Eduardo Caballero Calderón, Sec de Cultura y Turismo de Boyacá Tunja", "Entrada Libre"));
+
+							aniadirEntrada(new Lista_entrada("73","GRUPO CULTURAL LA CASA DE TESPIS. PERÚ", 
+								R.drawable.teatro, "4 de noviembre de 2014, 7:00 PM", 
+								"Teatro Cultural", "Entrada Libre"));
 			
 	}
 
